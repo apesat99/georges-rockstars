69196b85f062a869470466cf7dddc25aeca056b4
apesat99 <amyo.webdesign@gmail.com>
2020-05-08 11:17:33 +0100

    overdue commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("testing");
+
+// Mobile menu dropdown function
+
+$("#menu").click(function() {
+  $("#dropdown").toggle().css("display", "grid");
+}
+);
+
+// Mobile menu dropdown close on click outside
+
+$(document).mouseup(function(e){
+  var container = $("#dropdown");
+
+  if(!container.is(e.target) && container.has(e.target).length === 0){
+    container.hide();
+  }
+});
