4d50e58eadb30383b1d6e71eacd14c2c2a00a945
apesat99 <amyo.webdesign@gmail.com>
2020-05-14 17:52:25 +0100

    final commit

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ $(document).mouseup(function(e){
   }
 });
 
+// Donate button functions
 
 $("#donate").click(function() {
   window.open("https://www.gofundme.com/f/george039s-rockstars");
@@ -30,8 +31,117 @@ $("#donate-hero").click(function(){
   window.open("https://www.gofundme.com/f/george039s-rockstars");
 });
 
-$(document).ready(function () {
-    $( ".contact-button" ).click(function() {
-        $( ".contact-form" ).submit();
+// Contact form function
+
+$(document).ready(function(){
+  $(".form").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var submit = $("submit").val();
+
+    $(".form-message").load("mail.php",{
+      name: name,
+      email: email,
+      message: message,
+      submit: submit,
     });
+  });
 });
+
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    $('#contact-form').validator();
+
+
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    // data = JSON object that contact.php returns
+
+                    // we recieve the type of the message: success x danger and apply it to the
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
+/*$(function() {
+
+	// Get the form.
+	var form = $('#ajax-contact');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		});
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name, #email, #message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
+});*/
